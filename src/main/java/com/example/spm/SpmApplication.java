4f5853411098ca12038d122afd8d84a0d1f09ba7
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-04 23:22:01 +0900

    Init

---
+++ src/main/java/com/example/spm/SpmApplication.java
@@ -0,0 +1,13 @@
+package com.example.spm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spm/SpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
