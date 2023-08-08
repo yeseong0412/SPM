e331ab0da844b34a9baffc36d87f8740a4315769
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-08 20:50:14 +0900

    ItemControllerTest

---
+++ src/test/java/com/example/spm/ItemControllerTest.java
@@ -0,0 +1,18 @@
+package com.example.spm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class ItemControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+}
