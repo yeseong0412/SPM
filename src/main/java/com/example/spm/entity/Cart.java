1b4fc2a5fa566c75332f707b72162978dedb3975
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-09 16:33:16 +0900

    장바구니 Entity

---
+++ src/main/java/com/example/spm/entity/Cart.java
@@ -0,0 +1,22 @@
+package com.example.spm.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "Cart")
+@Getter
+@Setter
+@ToString
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
