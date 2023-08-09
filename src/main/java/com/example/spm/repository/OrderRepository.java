805a9e01575578cc47a1ebd6b4901bec4a102287
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-09 22:35:22 +0900

    Order 로직

--- src/main/java/com/example/spm/repository/OrderRepository.java
+++ src/main/java/com/example/spm/repository/OrderRepository.java
@@ -1,4 +1,8 @@
 package com.example.spm.repository;
 
-public interface OrderRepository{
+import com.example.spm.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
 }
