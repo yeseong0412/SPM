2bb22bbe9da56cf282108296db1f23d053da53ea
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-11 01:08:24 +0900

    Feat :: Test를 위한 List 추가

--- src/main/java/com/example/spm/repository/ItemImgRepository.java
+++ src/main/java/com/example/spm/repository/ItemImgRepository.java
@@ -3,5 +3,8 @@ package com.example.spm.repository;
 import com.example.spm.entity.ItemImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+    List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 }
