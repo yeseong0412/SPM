7e36a422878e7005942ea56da88ae62fb6099769
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-10 14:01:52 +0900

    Fix :: typo

--- src/main/java/com/example/spm/entity/BaseEntity.java
+++ src/main/java/com/example/spm/entity/BaseEntity.java
@@ -20,7 +20,7 @@ public abstract class BaseEntity extends BaseTimeEntity{
 
     @CreatedBy
     @Column(updatable = false)
-    private String createBy;
+    private String createdBy;
 
     @LastModifiedBy
     private String modifiedBy;
