ccad7f4d11c9f16d47ee4e2228006cd354771bab
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-10 23:28:26 +0900

    Feat :: Add ItemImgService Logic

---
+++ src/main/java/com/example/spm/service/ItemImgService.java
@@ -0,0 +1,18 @@
+package com.example.spm.service;
+
+import com.example.spm.repository.ItemImgRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ItemImgService {
+    @Value("${itemImgLocation}")
+    private String itemImgLocation;
+
+    private final ItemImgRepository itemImgRepository;
+    private final FileService fileService;
+}
