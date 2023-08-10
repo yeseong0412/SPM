7af2ee3f49416bbc0ee29bf96cb77f554731ba44
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-11 03:23:13 +0900

    Test :: Un Use Test Comments

--- src/test/java/com/example/spm/test/SpmApplicationTests.java
+++ src/test/java/com/example/spm/test/SpmApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.spm.test;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpmApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.spm.test;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpmApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
