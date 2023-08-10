842937804220ece4600db8b5534a9a29ed19e884
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-11 00:33:53 +0900

    Feat :: ItemController Connect ItemService

--- src/main/java/com/example/spm/controller/ItemController.java
+++ src/main/java/com/example/spm/controller/ItemController.java
@@ -1,12 +1,17 @@
 package com.example.spm.controller;
 
 import com.example.spm.dto.ItemFormDto;
+import com.example.spm.service.ItemService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+    private final ItemService itemService;
+
     @GetMapping(value = "/admin/item/new")
     public String itemForm(Model model){
         model.addAttribute("itemFormDto" , new ItemFormDto());
