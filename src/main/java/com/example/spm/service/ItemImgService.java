f83a9afc14e8128fae47b32362af65e7e8e7580e
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-11 02:32:26 +0900

    Feat :: Add Upload and Save Logic

--- src/main/java/com/example/spm/service/ItemImgService.java
+++ src/main/java/com/example/spm/service/ItemImgService.java
@@ -1,10 +1,15 @@
 package com.example.spm.service;
 
+import com.example.spm.entity.ItemImg;
 import com.example.spm.repository.ItemImgRepository;
+import io.micrometer.common.util.StringUtils;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -15,4 +20,23 @@ public class ItemImgService {
 
     private final ItemImgRepository itemImgRepository;
     private final FileService fileService;
+
+    public void saveItemImg(ItemImg itemimg, MultipartFile itemImgFile) throws Exception {
+
+        String oriImgName = itemImgFile.getOriginalFilename();
+        String imgName = "";
+        String imgUrl = "";
+
+        // 파일 업로드
+        if (!StringUtils.isEmpty(oriImgName)) {
+            imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+            imgUrl = "/images/item/" + imgName;
+        }
+
+
+        // 상품 이미지 정보 저장
+        itemimg.updateItemImg(oriImgName, imgName, imgUrl);
+        itemImgRepository.save(itemimg);
+    }
+
 }
