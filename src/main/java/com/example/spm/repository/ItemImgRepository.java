dac9165d95bbf46d02fcc68bd711eaacedb66ade
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-10 22:49:06 +0900

    Feat :: Add ItemImgRepository

---
+++ src/main/java/com/example/spm/repository/ItemImgRepository.java
@@ -0,0 +1,7 @@
+package com.example.spm.repository;
+
+import com.example.spm.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+}
