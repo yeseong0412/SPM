850612c69a62e7f11a73f5765fe891c7c478129a
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-10 16:49:13 +0900

    Fix :: Application += EnableJpaAuditing

--- src/main/java/com/example/spm/SpmApplication.java
+++ src/main/java/com/example/spm/SpmApplication.java
@@ -2,8 +2,10 @@ package com.example.spm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpmApplication {
 
     public static void main(String[] args) {
