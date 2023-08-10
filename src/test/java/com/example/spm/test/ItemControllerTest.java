fc73b84a851d11f53cee5034fa3e300dc721984e
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-10 20:53:51 +0900

    Fix :: Remove bug

--- src/test/java/com/example/spm/test/ItemControllerTest.java
+++ src/test/java/com/example/spm/test/ItemControllerTest.java
@@ -1,30 +1,35 @@
 package com.example.spm.test;
 
-import com.example.spm.test.annotation.TestInit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@TestInit
-public class ItemControllerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @Test
     @DisplayName("상품 등록 페이지 권한 테스트")
     @WithMockUser(username = "admin", roles = "ADMIN")
-    public void itemFormTest() throws Exception{
+    public void itemFormTest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/admin/item/new"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
+
     @Test
     @DisplayName("상품 등록 페이지 일반 회원 접근 테스트")
     @WithMockUser()
@@ -33,7 +38,4 @@ public class ItemControllerTest {
                 .andDo(print())
                 .andExpect(status().isForbidden());
     }
-
-
-
 }
