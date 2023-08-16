fd5ac69163e80c7d103530d052fc87dbca997168
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-17 08:38:30 +0900

    Feat :: Search Logic

---
+++ src/main/java/com/example/spm/dto/ItemSearchDto.java
@@ -0,0 +1,15 @@
+package com.example.spm.dto;
+
+
+import com.example.spm.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ItemSearchDto {
+
+    private String searchDateType;
+    private ItemSellStatus searchSellStatus;
+    private String searchBy;
+    private String searchQuery = "";
+}
---
+++ src/main/java/com/example/spm/repository/ItemRepositoryCustom.java
@@ -0,0 +1,16 @@
+package com.example.spm.repository;
+
+import com.example.spm.dto.ItemSearchDto;
+import com.example.spm.dto.MainItemDto;
+import com.example.spm.entity.Item;
+import org.springframework.data.domain.Page;
+
+import java.awt.print.Pageable;
+
+public interface ItemRepositoryCustom {
+
+    Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+    // @QueryProjection 을 이용하여 바로 Dto 객체 반환
+    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+}
