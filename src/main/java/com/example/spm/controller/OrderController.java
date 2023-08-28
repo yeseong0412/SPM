550db5849d304dd8de4335d00c99247bff953f37
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-28 23:10:14 +0900

    Feat :: Add orderHist

--- src/main/java/com/example/spm/controller/OrderController.java
+++ src/main/java/com/example/spm/controller/OrderController.java
@@ -2,21 +2,25 @@ package com.example.spm.controller;
 
 
 import com.example.spm.dto.OrderDto;
+import com.example.spm.dto.OrderHistDto;
 import com.example.spm.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -48,6 +52,21 @@ public class OrderController {
         }
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);
     }
+
+    @GetMapping(value = {"/order" , "order/{page}"})
+    public String orderHist(@PathVariable("page") Optional<Integer> page, Principal principal, Model model){
+
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 4);
+
+        Page<OrderHistDto> orderHistDtoList = orderService.getOrderList(principal.getName(), pageable);
+
+        model.addAttribute("orders", orderHistDtoList);
+        model.addAttribute("page" , pageable);
+        model.addAttribute("maxPage" , 5);
+
+        return "order/orderHist";
+    }
+
 
 
 }
